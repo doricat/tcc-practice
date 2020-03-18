2a0cd732e64e33ec3f1caaffae5f52245e068cda
doricat <doricat@outlook.com>
2020-03-18 23:44:07 +0800

    数据库脚本

--- order.sql
+++ order.sql
@@ -1,14 +1,12 @@
-create database order_db;
-
 create table orders
 (
-    id         bigint    not null primary key,
-    user_id    bigint    not null,
-    bill_id    bigint    not null,
-    state      int       not null,
-    created_at timestamp not null,
-    expires    timestamp not null,
-    updated_at timestamp not null
+    id             bigint      not null primary key,
+    user_id        bigint      not null,
+    transaction_id bigint      not null,
+    state          varchar(20) not null,
+    created_at     timestamp   not null,
+    expires        timestamp   not null,
+    updated_at     timestamp   not null
 );
 
 create table order_items
@@ -18,15 +16,68 @@ create table order_items
     product_id bigint         not null,
     price      decimal(10, 2) not null,
     qty        int            not null,
-    foreign key (product_id) references products (id),
     foreign key (order_id) references orders (id)
 );
 
-create table shopping_cars
-(
-    id         bigint not null primary key,
-    user_id    bigint not null,
-    product_id bigint not null,
-    qty        int    not null,
-    foreign key (product_id) references products (id)
-);
+create or replace function confirm_order(_transaction_id bigint, _updated_at timestamp)
+    returns bool
+as
+$func$
+declare
+    __state   varchar(20);
+    __expires timestamp;
+begin
+    select state, expires into __state, __expires from orders where transaction_id = _transaction_id for update;
+
+    if __state <> 'Pending' or __expires < _updated_at then
+        return false;
+    end if;
+
+    update orders set state = 'Confirmed', updated_at = _updated_at where transaction_id = _transaction_id;
+    return true;
+end;
+$func$ language plpgsql;
+
+create or replace function cancel_order(_transaction_id bigint, _updated_at timestamp)
+    returns bool
+as
+$func$
+declare
+    __state      varchar(20);
+    __expires    timestamp;
+begin
+    select state, expires
+    into __state, __expires
+    from orders
+    where transaction_id = _transaction_id for update;
+
+    if __state <> 'Pending' or __expires < _updated_at then
+        return false;
+    end if;
+
+    update orders set state = 'Canceled', updated_at = _updated_at where transaction_id = _transaction_id;
+
+    return true;
+end;
+$func$ language plpgsql;
+
+create or replace function notify_order_state()
+    returns trigger
+as
+$func$
+begin
+    perform pg_notify('transaction_channel', json_build_object(
+                                                     'id', new.transaction_id,
+                                                     'state', new.state,
+                                                     'beginTime', new.created_at,
+                                                     'expires', new.expires
+                                                 ) #>> '{}');
+    return new;
+end;
+$func$ language plpgsql;
+
+create trigger state_trigger
+    after insert or update
+    on orders
+    for each row
+execute function notify_order_state();
---
+++ payment.sql
@@ -0,0 +1,111 @@
+create table accounts
+(
+    id      bigint         not null primary key,
+    user_id bigint         not null unique,
+    balance decimal(10, 2) not null default 0
+);
+
+create table bills
+(
+    id             bigint         not null primary key,
+    account_id     bigint         not null,
+    amount         decimal(10, 2) not null,
+    transaction_id bigint         not null,
+    state          varchar(20)    not null,
+    created_at     timestamp      not null,
+    expires        timestamp      not null,
+    updated_at     timestamp      not null,
+    foreign key (account_id) references accounts (id)
+);
+
+create or replace function create_bill(_id bigint,
+                                       _user_id bigint,
+                                       _amount decimal(10, 2),
+                                       _transaction_id bigint,
+                                       _created_at timestamp,
+                                       _expires timestamp)
+    returns int /*0=Ok; 1=InsufficientBalance*/
+as
+$func$
+declare
+    __balance    decimal(10, 2);
+    __account_id bigint;
+begin
+    select balance, id into __balance, __account_id from accounts where user_id = _user_id for update;
+    if __balance + _amount < 0 then
+        return 1;
+    end if;
+
+    update accounts set balance = balance + _amount where user_id = _user_id;
+    insert into bills(id, account_id, amount, transaction_id, state, created_at, expires, updated_at)
+    values (_id, __account_id, _amount, _transaction_id, 'Pending', _created_at, _expires, _created_at);
+
+    return 0;
+end;
+$func$ language plpgsql;
+
+create or replace function confirm_bill(_transaction_id bigint, _updated_at timestamp)
+    returns bool
+as
+$func$
+declare
+    __state   varchar(20);
+    __expires timestamp;
+begin
+    select state, expires into __state, __expires from bills where transaction_id = _transaction_id for update;
+    if __state <> 'Pending' or __expires < _updated_at then
+        return false;
+    end if;
+
+    update bills set state = 'Confirmed', updated_at = _updated_at where transaction_id = _transaction_id;
+    return true;
+end;
+$func$ language plpgsql;
+
+create or replace function cancel_bill(_transaction_id bigint, _updated_at timestamp)
+    returns bool
+as
+$func$
+declare
+    __account_id bigint;
+    __amount     decimal(10, 2);
+    __state      varchar(20);
+    __expires    timestamp;
+begin
+    select account_id, amount, state, expires
+    into __account_id, __amount, __state, __expires
+    from bills
+    where transaction_id = _transaction_id for update;
+
+    if __state <> 'Pending' or __expires < _updated_at then
+        return false;
+    end if;
+
+    update bills set state = 'Canceled', updated_at = _updated_at where transaction_id = _transaction_id;
+    update accounts set balance = balance + abs(__amount) where id = __account_id;
+    return true;
+end;
+$func$ language plpgsql;
+
+create or replace function notify_bill_state()
+    returns trigger
+as
+$func$
+begin
+    perform pg_notify('transaction_channel', json_build_object(
+                                                     'id', new.transaction_id,
+                                                     'state', new.state,
+                                                     'beginTime', new.created_at,
+                                                     'expires', new.expires
+                                                 ) #>> '{}');
+    return new;
+end;
+$func$ language plpgsql;
+
+create trigger state_trigger
+    after insert or update
+    on bills
+    for each row
+execute function notify_bill_state();
+
+insert into accounts(id, user_id, balance) values (11704070658064300, 11704070658064385, 1000);
